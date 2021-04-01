2e54c51439243995d56596c1297f37accfbde9b0
Adam Richards <adamwilliamrichards@gmail.com>
2021-04-01 11:58:07 +0100

    added code to sedn user to the home page

--- app.js
+++ app.js
@@ -9,7 +9,7 @@ const port = 8080;
 app.use(express.static('public'))
 app.use(express.static('js'))
 
-app.listen(port, (err) => {
+app.listen(port, (err) => {†
     console.log(`server listening on http://localhost:${port}`)
 });
 

c60cfb8a60dc8f1bb543daea6f6cae81be7ca76e
Adam Richards <adamwilliamrichards@gmail.com>
2021-04-20 16:13:43 +0100

    changed sever

--- app.js
+++ app.js
@@ -8,7 +8,6 @@ const port = 8080;
 
 app.use(express.static('public'))
 app.use(express.static('js'))
-app.use(express.static(__dirname))
 
 app.listen(port, (err) => {
     console.log(`server listening on http://localhost:${port}`)
